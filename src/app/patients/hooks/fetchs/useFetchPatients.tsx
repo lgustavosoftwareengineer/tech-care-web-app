83ae38312d7230384fd08d3a1ca46dbdc1a39ddf
Luiz Gustavo <gugamatias7@gmail.com>
2024-07-19 19:29:12 -0300

    feat: use swr to make patients fetch

--- src/app/patients/hooks/fetchs/useFetchPatients.tsx
+++ src/app/patients/hooks/fetchs/useFetchPatients.tsx
@@ -1,61 +1,27 @@
 import { EnvironmentVariables } from "@/app/configs";
 import { Patient } from "@/app/types";
-import { useState, useEffect } from "react";
-import { create } from "zustand";
+import useSWR from "swr";
+
+const fetchPatients = async () => {
+  const credentials = btoa(
+    `${EnvironmentVariables.API_USERNAME}:${EnvironmentVariables.API_PASSWORD}`
+  );
+
+  const response = await fetch(EnvironmentVariables.API_URL, {
+    headers: {
+      Authorization: `Basic ${credentials}`,
+    },
+  });
+
+  return response.json();
+};
 
 export function useFetchPatients() {
-  const { isLoading, patients, setIsLoading, setPatients } = usePatients();
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchPatients = async () => {
-      const credentials = btoa(
-        `${EnvironmentVariables.API_USERNAME}:${EnvironmentVariables.API_PASSWORD}`
-      );
-
-      try {
-        const response = await fetch(EnvironmentVariables.API_URL, {
-          headers: {
-            Authorization: `Basic ${credentials}`,
-          },
-        });
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data: Patient[] = await response.json();
-        setPatients(data);
-      } catch (error: any) {
-        setError(error.error_message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    if (!patients.length) {
-      fetchPatients();
-    }
-  }, [patients, setIsLoading, setPatients]);
+  const {
+    data: patients,
+    isLoading,
+    error,
+  } = useSWR<Patient[]>("fetch-patients", fetchPatients);
 
   return { patients, isLoading, error };
 }
-
-type State = {
-  patients: Patient[];
-  isLoading: boolean;
-};
-
-type Actions = {
-  setPatients: (patients: Patient[]) => void;
-  setIsLoading: (loading: boolean) => void;
-};
-
-type UserStore = State & Actions;
-
-const INITIAL_STATE: Patient[] = [];
-
-const usePatients = create<UserStore, any>((set) => ({
-  patients: INITIAL_STATE,
-  isLoading: true,
-  setPatients: (patients) => set(() => ({ patients })),
-  setIsLoading: (isLoading) => set(() => ({ isLoading })),
-}));
